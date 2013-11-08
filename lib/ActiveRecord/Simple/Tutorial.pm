c57c3ff69c82ff94b8c2da707c2264c7f4786de9
Alexander Ponomarev <ap@webzilla.com>
2013-11-08 17:24:33 +0200

    fix tutorial

--- lib/ActiveRecord/Simple/Tutorial.pm
+++ lib/ActiveRecord/Simple/Tutorial.pm
@@ -14,11 +14,11 @@ ActiveRecord::Simple::Tutorial
 
 =head1 DESCRIPTION
 
-Using ActiveRecord::Simple as an ORMapper in your project
+Information how to use ActiveRecord::Simple as an ORMapper in your project.
 
 =head1 INTRO
 
-Before we start, you should to know few issues about ActiveRecord::Simple
+Before we start, you should to know a few issues about ActiveRecord::Simple
 
 =over 8
 
@@ -38,6 +38,8 @@ Perl doesn't check types. ARSimple is like perl.
 
 =head1 PROJECT's FILE SYSTEM
 
+Filesystem of the future project looks like that.
+
 =over 8
 
 =item -B<sql>
@@ -56,7 +58,7 @@ Tests.
 
 =head1 DATABASE DESIGN
 
-We going to make yet another blog engine and need a simple model: authors, articles and comments.
+We're going to make yet another blog engine and create a simple model: authors, articles and comments.
 
     CREATE TABLE authors (
         id   INTEGER PRIMARY KEY,
@@ -84,44 +86,49 @@ Save this SQL-code in "sql/myschema.sql" and create the sqlite database:
 
 =head1 CLASSES
 
-To generate classes, run "arsimple" script:
+To generate classes, run "mars" script:
 
-    $ cd lib && arsimple -perl -dir ../sql -driver SQLite
+    $ cd lib && mars -perl -dir ../sql -driver SQLite
 
 The script will read recursively directory, find the .sql files and create perl-classes from sql.
-Now we have three files: Articles.pm, Authors.pm and Comments.pm. Let's do tests.
+Now we have three files: Articles.pm, Authors.pm and Comments.pm. Let's start the test.
 
 =head1 BASIC SYNTAX
 
-In first test we should create authors. Let's go to t/ and create test-file authors.t:
+In first test we have to create authors. Let's go to t/ and create test-file authors.t:
 
     $ touch authors.t
 
-You should create database handler and insert into Authors->dbh():
+You have to create database handler and insert it into Authors->dbh():
 
     use DBI;
     use Authors;
 
     Authors->dbh(DBI->connect("dbi:SQLite:sql/blog.db"));
 
-So, we are ready to write our first bunch of tests. First, create authors John Doe and Jack Black:
+So, we are ready to write our first bunch of tests. First, let's create authors John Doe and Jack Black:
 
     ok Authors->new({ name => 'John Doe' })->save;
     ok Authors->new({ name => 'Jack Black' })->save;
 
-Second, check each author has been saved in database:
+Second, check each author has been saved in the database:
 
     ok my $john = Authors->find({ name => 'John Doe' })->fetch;
-    ok $john->is_defined; is $john->name, 'John Doe'; is $john->id, 1;
+    ok $john->is_defined;
+    is $john->name, 'John Doe';
+    is $john->id, 1;
+
     ok my $jack = Authors->find({ name => 'Jack Black' })->fetch;
-    ok $jack->is_defined; is $jack->name, 'Jack Black'; is $jack->id, 2;
+    ok $jack->is_defined;
+    is $jack->name, 'Jack Black';
+    is $jack->id, 2;
 
 We have done with authors. Let's create articles.
 
 =head1 RELATIONS
 
 As you can see, table "articles" belongs to "authors", this is relation one-to-many: one author
-can have many articles, but only one article beongs to one author. We have to reflect it in the code.
+can have many articles, but only one article belongs to one author. We have to reflect it in the code.
 In Authors.pm (one-to-many):
 
     __PACKAGE__->relations({
@@ -160,14 +167,14 @@ Now check the article has been saved and linked to the author:
     my $first_article = shift @articles;
     is $first_article->title, 'My first article';
 
-Also we can change an author of the article, simply set it into accessor:
+Also we can change article's author. Just put new object into accessor "author":
 
     ok $first_article->author(Authors->find({ name => 'Jack Black' })->fetch)->save;
     is $first_article->author->name, 'Jack Black';
 
 =head1 SQL-MODIFIERS
 
-ARSimple provides a few sql-modifiers: order_by, asc, desc, limit, offset. All this modifiers you can use after call "fetch".
+ARSimple provides a few sql-modifiers: order_by, asc, desc, limit, offset. All this modifiers you can use before call "fetch".
 Let's create test "comments" and take a look a bit closer to that functions. Of course, we need to add new relations to Articles and Comments classes to use it in our tests. I think, you already know how to do it ;-)
 
     my $article = Articles->get(1); ### it's the same as Articles->find(1)->fetch;
